baea0b55aad9aeb307c0e22d52172d1194fe1dfd
anton <rkzton@yandex.ru>
2022-12-18 15:37:28 +0300

    lesson_2

--- simple_wsgi.py
+++ simple_wsgi.py
@@ -1,62 +1,90 @@
 '''
-Скрыть
 В этой самостоятельной работе тренируем умения:
 
-    Использовать паттерны page controller, front controller
-    Использовать шаблонизатор
+    Разделять get и post запрос внутри wsgi-фреймворка
+    Получать и декодировать параметры post запроса
 
 Смысл:
 
-Понимать и применять паттерны page и front controllers, понимать как устроены и работают wsgi фреймворки. Использовать шаблонизаторы
+Чтобы уметь обрабатывать разные типы web-запросов
 Последовательность действий:
-0. Создать репозиторий для нового проекта (gitlab, github, ...)
-1. С помощью uwsgi или gunicorn запустить пример simple_wsgi.py, проверить что он работает (Эти библиотеки работают на linux системах, документацию по ним можно найти в дополнительных материалах)
-2. Написать свой wsgi фреймворк использую паттерны page controller и front controller.
-Описание работы фреймворка:
+0. Добавить в свой wsgi-фреймворк возможность обработки post-запроса
+1. Добавить в свой wsgi-фреймворк возможность получения данных из post запроса
+2. Дополнительно можно добавить возможность получения данных из get запроса
+3. В проект добавить страницу контактов на которой пользователь может отправить нам сообщение (пользователь вводит тему сообщения, его текст, свой email)
+4. После отправки реализовать сохранение сообщения в файл, либо вывести сообщение в терминал (базу данных пока не используем)
 
-    возможность отвечать на get запросы пользователя (код ответа + html страница)
-    для разных url - адресов отвечать разными страницами
-    page controller - возможность без изменения фреймворка добавить view для обработки нового адреса
-    front controller - возможность без изменения фреймворка вносить изменения в обработку всех запросов
-
-3. Реализовать рендеринг страниц с помощью шаблонизатора jinja2. Документацию по этой библиотеке можно найти в дополнительных материалах
-4. Добавить любый полезный функционал в фреймворк, например обработку наличия (отсутствия) слеша в конце адреса, ...
-5. Добавить для демонстрации 2 любые разные страницы (например главная и about или любые другие)
-6. Сдать дз в виде ссылки на репозиторий
-7. В readme указать пример как запустить фреймворк с помощью uwsgi и/или gunicorn
 
 '''
-
+from quopri import decodestring
 
 from jinja2 import Template
 from pprint import pprint
 
 
-def application(environ, start_response):
-    #pprint(environ)
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    site = front_controller(environ['PATH_INFO'])
-    return [site.encode('utf-8')]
+class Application:
+    def __init__(self, urls):
+        self.urls = urls
 
+    def __call__(self, environ, start_response):
+        site = self.front_controller(environ['PATH_INFO'], environ)
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [site.encode('utf-8')]
 
-def front_controller(path):
-    if path in URLS:
-        content = URLS[path]
-    else:
-        content = page_controller_404()
-    return content
+    def front_controller(self, path, env):
+        method = env['REQUEST_METHOD']
+        query = env["QUERY_STRING"]
+        if method == 'GET':
+            if query:
+                print(f'Получен GET запрос, c данными {self.parse_query_data(query)}')
+            else:
+                print('Получен GET запрос, без данных')
+        elif method == 'POST':
+            if env.get('CONTENT_LENGTH'):
+                message = self.post_request(env)
+                print(f'Получено сообщение (POST запрос) от {message["name"]}, c адресом {message["email"]}\n'
+                      f'Cообщение: {message["message"]}')
+        if path in self.urls:
+            content = self.urls[path]
+        else:
+            content = page_controller_404()
+        return content
+
+    @staticmethod
+    def parse_query_data(data):
+        result = {}
+        params = data.split('&')
+        for param in params:
+            key, value = param.split('=')
+            result[key] = value
+        return result
+
+    def post_request(self, env):
+        data = env['wsgi.input'].read(int(env.get('CONTENT_LENGTH')))
+        data = data.decode(encoding='utf-8')
+        data = self.parse_query_data(data)
+        data = self.decode_value(data)
+        return data
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace('+', ' '), 'UTF-8')
+            new_data[k] = decodestring(val).decode('UTF-8')
+        return new_data
 
 
 def page_controller_main():
-    return render('index.html', object_list=[{'content': 'Main Page'}])
+    return render('templates/index.html', object_list=[{'content': 'Main Page'}])
 
 
 def page_controller_about():
-    return render('index.html', object_list=[{'content': 'About us'}])
+    return render('templates/about.html', object_list=[{'content': 'About us'}])
 
 
 def page_controller_contacts():
-    return render('index.html', object_list=[{'content': 'Contacts'}])
+    return render('templates/contacts.html', object_list=[{'content': 'Contacts'}])
 
 
 def page_controller_404():
@@ -74,3 +102,5 @@ URLS = {
     '/about': page_controller_about(),
     '/contacts': page_controller_contacts(),
 }
+
+application = Application(URLS)
